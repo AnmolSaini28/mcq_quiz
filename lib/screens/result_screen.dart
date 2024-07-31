f3b1e208820c8462d45e4d681d631860791a1c92
AnmolSaini28 <sainianmol1507@gmail.com>
2024-07-31 11:53:06 +0530

    fix bug

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -31,7 +31,7 @@ class ResultScreen extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             Text(
-              '3 / $totalQuestions',
+              '$score / $totalQuestions',
               style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
             ),
             const SizedBox(height: 40),
